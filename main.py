871ab906c11b68c817fc4812c074b2a2004d23d2
Gresliebear <lesliemwubbel@gmail.com>
2021-01-22 11:42:15 -0500

    Pushing results

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from utils.crops_methods.compute_3D_bbs_from_gt_liver import compute_3D_bbs_from
 from utils.sampling_bb.sample_bbs import sample_bbs_test, sample_bbs_train
 from det_lesion_test import det_lesion_test
 from seg_lesion_test import seg_lesion_test
+import time
+
 
 class LiverLesion:
     def __init__(self, config, number_slices=3):
@@ -45,18 +47,20 @@ class LiverLesion:
         """
             Driver code for testing the model.
         """
-
+        
         test_steps = [
-            #['seg_liver_test', self.seg_liver_test],
-            #['compute_bbs_from_gt_liver', self.compute_3D_bbs_from_gt_liver], 
-            #['sample_bbs_test', self.sample_bbs_test], 
+            ['seg_liver_test', self.seg_liver_test],
+            ['compute_bbs_from_gt_liver', self.compute_3D_bbs_from_gt_liver], 
+            ['sample_bbs_test', self.sample_bbs_test], 
             ['det_lesion_test', self.det_lesion_test], 
-            #['seg_lesion_test', self.seg_lesion_test]
+            ['seg_lesion_test', self.seg_lesion_test]
         ]
         for name, step in test_steps:
             print('Running step: '+ name)
+            start_time = time.time()
             step()
             print('Done step: '+ name)
+            print ("My program took", time.time() - start_time, "to run")
 
 if __name__ =='__main__':
     from config import Config
