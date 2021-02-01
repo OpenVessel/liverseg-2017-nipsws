712b55ed44861a6f4b957c20939a1ca5f953f50f
rishyak <rishyak@gmail.com>
2021-02-01 14:55:27 -0500

    Finished TO-DO 1

--- utils/initial_txt_utils/make_test_train_txt.py
+++ utils/initial_txt_utils/make_test_train_txt.py
@@ -23,22 +23,16 @@ images_volumes_fold = os.path.join(root_dataset, images_volumes)
 images_volumes_patID = os.listdir(images_volumes_fold)
 images_volumes_patID.sort()
 images_volumes_patID = sorted(images_volumes_patID, key=len)
-print(images_volumes_patID)
-print()
 
 item_seg_fold = os.path.join(root_dataset, item_seg)
 item_seg_patID = os.listdir(item_seg_fold)
 item_seg_patID.sort()
 item_seg_patID = sorted(item_seg_patID, key=len)
-print(item_seg_patID)
-print()
 
 liver_seg_fold = os.path.join(root_dataset, liver_seg)
 liver_seg_patID = os.listdir(liver_seg_fold)
 liver_seg_patID.sort()
 liver_seg_patID = sorted(liver_seg_patID, key=len)
-print(liver_seg_patID)
-print()
 
 
 all_paths = []
@@ -53,7 +47,8 @@ for i in range(len(images_volumes_patID) - 1):
         patient_slice.sort()
         patient_slice = sorted(patient_slice, key=len)
         for pat_slice in patient_slice:
-            pat_slice_path = os.path.join(patient_path, pat_slice)
+            pat_slice_path = os.path.join(patient_path, pat_slice).split(os.path.sep)[3:]
+            pat_slice_path = os.path.sep.join(pat_slice_path)
             all_patient_data[0].append(pat_slice_path)
 
     if item_seg_patID[i] != ".DS_Store":
@@ -62,7 +57,8 @@ for i in range(len(images_volumes_patID) - 1):
         patient_slice.sort()
         patient_slice = sorted(patient_slice, key=len)
         for pat_slice in patient_slice:
-            pat_slice_path = os.path.join(patient_path, pat_slice)
+            pat_slice_path = os.path.join(patient_path, pat_slice).split(os.path.sep)[3:]
+            pat_slice_path = os.path.sep.join(pat_slice_path)
             all_patient_data[1].append(pat_slice_path)
 
     if liver_seg_patID[i] != ".DS_Store":
@@ -71,17 +67,18 @@ for i in range(len(images_volumes_patID) - 1):
         patient_slice.sort()
         patient_slice = sorted(patient_slice, key=len)
         for pat_slice in patient_slice:
-            pat_slice_path = os.path.join(patient_path, pat_slice)
+            pat_slice_path = os.path.join(patient_path, pat_slice).split(os.path.sep)[3:]
+            pat_slice_path = os.path.sep.join(pat_slice_path)
             all_patient_data[2].append(pat_slice_path)
 
     all_paths.append(all_patient_data)
 
-print(all_paths[0])
-print(all_paths[1])
-print(all_paths[2])
-
+# all_paths = [pat0, pat1, pat2, pat3, pat4, ..., pat131]
+    # patXYZ = [[images_volumes], [item_seg], [liver_seg]]
+        # images_volumes = all paths for patientXYZ from that folder
+        #       item_seg = all paths for patientXYZ from that folder
+        #      liver_seg = all paths for patientXYZ from that folder
 
 # TO-DO
-    # Need to think of a way to remove the ../../LiTS_databse
     # Make output txt file
     # Easy peasy
