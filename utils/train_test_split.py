43734a36ebd10a5631a4fd0c25eefab0e9f44a43
Gregory Glatzer <gregoryg323@gmail.com>
2021-03-12 15:43:05 -0500

    Cleaned up train test split. Still doesn't work

--- utils/train_test_split.py
+++ utils/train_test_split.py
@@ -19,45 +19,46 @@ class TrainTestSplit:
         return: list of lists that will be fed into a pandas DataFrame
         """
 
+        def get_sorted_dir(dir):
+            dirContents = os.listdir(dir)
+            dirContents.sort()
+            return sorted(dirContents, key=len)
+
 
         # ground truths
         images_volumes_fold = os.path.join(self.config.database_root, images_volumes)
-        images_volumes_patID = os.listdir(images_volumes_fold)
-        images_volumes_patID.sort()
-        images_volumes_patID = sorted(images_volumes_patID, key=len)
+        images_volumes_patID = get_sorted_dir(images_volumes_fold)
 
         # lesions
         item_seg_fold = os.path.join(self.config.database_root, item_seg)
-        item_seg_patID = os.listdir(item_seg_fold)
-        item_seg_patID.sort()
-        item_seg_patID = sorted(item_seg_patID, key=len)
+        item_seg_patID = get_sorted_dir(item_seg_fold)
 
         # liver
         liver_seg_fold = os.path.join(self.config.database_root, liver_seg)
-        liver_seg_patID = os.listdir(liver_seg_fold)
-        liver_seg_patID.sort()
-        liver_seg_patID = sorted(liver_seg_patID, key=len)
+        liver_seg_patID = get_sorted_dir(liver_seg_fold)
 
         all_paths = []
 
-        for i in range(len(images_volumes_patID) - 1):
-
+        for i, patientID in enumerate(images_volumes_patID): # loop through each patient
+            if self.config.debug:
+                print('(Train Test Split) Sorting patient {}'.format(patientID))
+            
             all_patient_data = [[], [], []]
 
             for j, id in enumerate([images_volumes_patID, item_seg_patID, liver_seg_patID]):
                 if id[i] != ".DS_Store":
+                    
                     patient_path = os.path.join(images_volumes_fold, images_volumes_patID[i])
-                    patient_slice = os.listdir(patient_path)
-                    patient_slice.sort()
-                    patient_slice = sorted(patient_slice, key=len)
-                    for pat_slice in patient_slice:
+                    patient_slices = get_sorted_dir(patient_path)
+
+                    for pat_slice in patient_slices:
                         pat_slice_path = os.path.join(patient_path, pat_slice).split(os.path.sep)[self.config.num_slices:]
                         pat_slice_path = os.path.sep.join(pat_slice_path)
                         all_patient_data[j].append(pat_slice_path)
 
             all_paths.append(all_patient_data)
 
-        return all_paths
+        return all_paths[0]
 
 
     def get_data_volume(self, lol):
@@ -70,10 +71,7 @@ class TrainTestSplit:
             for patSlice in range(num_patSlices):
                 row = []
                 for i in range(self.config.num_slices):
-                    row_segment = []
-                    for j in range(self.config.num_slices):
-                        row_segment.append( str(lol[pat][j][patSlice] ) )
-                    row.append(" ".join(row_segment))
+                    row.append( str(lol[pat][patSlice+i]) )
                 rows.append(row)
         return pd.DataFrame(rows, columns=['slice {}'.format(i) for i in range(1, self.config.num_slices+1)])
 
@@ -90,10 +88,26 @@ class TrainTestSplit:
         """
         
         lol = self.sort_list(images_volumes, item_seg, liver_seg)
+
         # split data
-        print(np.array(lol)[0][1])
-        split_point = math.floor(np.array(lol).shape[-1]*train_test_split_ratio)
-        training_volume = self.get_data_volume(lol[split_point:])
-        testing_volume = self.get_data_volume(lol[:split_point])
+        num_patients = np.array(lol).shape[0]
+        split_point = int(math.floor( num_patients * train_test_split_ratio )) - 1
+        training_set = lol[:split_point]
+        testing_set = lol[split_point:]
+
+        print('')
+        print('# patients in dataset: {}'.format(num_patients))
+        print('Splitting on patient {}'.format(split_point))
+        print('-'*40)
+        print('First row of training set')
+        print(training_set[0][0]) # patient 0, row 0
+        print('')
+        print('First row of testing set')
+        print(testing_set[0][0])
+        print('-'*40)
+
+
+        training_volume = self.get_data_volume(training_set)
+        testing_volume = self.get_data_volume(testing_set)
 
         return training_volume, testing_volume
